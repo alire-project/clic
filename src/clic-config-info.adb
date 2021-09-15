3559e751d32196bdb268f3b0bb099850c95f7b43
Fabien Chouteau <chouteau@adacore.com>
2021-09-15 17:13:46 +0200

    CLIC.Config.Info: add a List_Keys function and fix filtering
    The filtering mixed regex (default Filter pattern) and globing (actually system user in List).
    Everything is now based on regex and documented.

--- src/clic-config-info.adb
+++ src/clic-config-info.adb
@@ -1,6 +1,6 @@
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 
-with GNAT.Regexp;
+with GNAT.Regpat;
 
 package body CLIC.Config.Info is
 
@@ -13,31 +13,51 @@ package body CLIC.Config.Info is
                   Show_Origin : Boolean := False)
                   return AAA.Strings.Vector
    is
-      use GNAT.Regexp;
-
-      Re     : constant Regexp := Compile (Filter, Glob => True);
-
       Result : AAA.Strings.Vector;
    begin
-      for C in This.Config_Map.Iterate loop
+
+      for Key of List_Keys (This, Filter) loop
          declare
-            Val : constant Config_Value := This.Config_Map (C);
-            Key : constant String := To_String (Config_Maps.Key (C));
+            Val : constant Config_Value := This.Config_Map (+Key);
             Line : Unbounded_String;
          begin
-            if Match (Key, Re) then
-               if Show_Origin then
-                  Line := Val.Origin & ": ";
-               end if;
-
-               Append (Line, Key & "=");
-               Append (Line, Image (Val.Value));
-               Result.Append (To_String (Line));
+            if Show_Origin then
+               Line := Val.Origin & ": ";
             end if;
+
+            Append (Line, Key & "=");
+            Append (Line, Image (Val.Value));
+            Result.Append (To_String (Line));
          end;
       end loop;
 
       return Result;
    end List;
+
+   ---------------
+   -- List_Keys --
+   ---------------
+
+   function List_Keys (This        : CLIC.Config.Instance;
+                       Filter      : String := ".*")
+                       return AAA.Strings.Vector
+   is
+      use GNAT.Regpat;
+
+      Re     : constant Pattern_Matcher := Compile (Filter);
+
+      Result : AAA.Strings.Vector;
+   begin
+      for C in This.Config_Map.Iterate loop
+         declare
+            Key : constant String := To_String (Config_Maps.Key (C));
+         begin
+            if Match (Re, Key) then
+               Result.Append (Key);
+            end if;
+         end;
+      end loop;
+      return Result;
+   end List_Keys;
 
 end CLIC.Config.Info;
