b5b02eafbea8058f1f4ea0dc453af0c6a6d070fe
Fabien Chouteau <chouteau@adacore.com>
2021-09-15 17:13:46 +0200

    CLIC.Config.Info: add a List_Keys function and fix filtering
    The filtering mixed regex (default Filter pattern) and globing (actually system user in List).
    Everything is now based on regex and documented.

--- src/clic-config-info.ads
+++ src/clic-config-info.ads
@@ -7,5 +7,13 @@ package CLIC.Config.Info is
    --  Return a Vector of String that contains a list of configuration
    --  key/value as seen in the configuration. When Show_Origin is true,
    --  the configuration file where each key was loaded is also listed.
+   --
+   --  The keys not matching the Filter regular expression (see GNAT.Regpat)
+   --  are ignored.
+
+   function List_Keys (This        : CLIC.Config.Instance;
+                       Filter      : String := ".*")
+                       return AAA.Strings.Vector;
+   --  Same as above but only return the config keys
 
 end CLIC.Config.Info;
